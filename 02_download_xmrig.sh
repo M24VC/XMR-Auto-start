2e8a7388e12dca9f9b2b017f06c2c27a9d1ecc0b
M24VC <chiangvin007@gmail.com>
2025-12-15 10:02:55 +0800

    update 02_download_xmrig.sh

--- 02_download_xmrig.sh
+++ 02_download_xmrig.sh
@@ -1,9 +1,3 @@
-#!/bin/bash
-cd "$(dirname "$0")/xmrig"
-
-ARCH=$(uname -m)
-[ "$ARCH" != "x86_64" ] && exit 0
-
-wget -q https://github.com/xmrig/xmrig/releases/latest/download/xmrig-6.22.0-linux-x64.tar.gz -O xmrig.tgz
+wget -q -O xmrig.tgz https://github.com/xmrig/xmrig/releases/download/v6.22.0/xmrig-6.22.0-linux-x64.tar.gz
 tar -xzf xmrig.tgz --strip-components=1
 chmod +x xmrig
